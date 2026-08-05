039ebe273ee19f8c35b521309650d0e2008d8136
mrebukks <obiebukaugonna@gmail.com>
2026-08-05 18:06:48 +0100

    Stupid error made in uploads.js with cloudinary api

--- config/uploads.js
+++ config/uploads.js
@@ -6,7 +6,7 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 // 1. Configure Cloudinary with your environment keys
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
+  api_key: process.env.CLOUD_API,
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
